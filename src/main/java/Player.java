adf4c9e959d1e92972888e8399078285ed1cae78
Duncan Ryan <duncancryan1@gmail.com>
2020-09-24 14:56:04 +0100

    player constructed

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,12 @@
 public class Player {
+
+    private Card card;
+
+    public Player(){
+        this.card = null;
+    }
+
+    public Card getCard() {
+        return card;
+    }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlayerTest {
+
+    private Dealer dealer;
+    private Player player1;
+    private Player player2;
+    private Deck deck;
+
+    @Before
+    public void before() {
+        deck = new Deck();
+        deck.populateDeck();
+        player1 = new Player();
+        player2 = new Player();
+        dealer = new Dealer(deck);
+    }
+
+    @Test
+    public void handStartsAsNull(){
+        assertEquals(null, player1.getCard());
+    }
+
 }
