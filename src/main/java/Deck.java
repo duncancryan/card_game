9393d6b1ff63aa36e7daa6853858081088f859d6
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-24 10:59:58 +0100

    creates empty deck of cards and test

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<>();
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public int getNumberOfCards(){
+        return cards.size();
+    }
 }
