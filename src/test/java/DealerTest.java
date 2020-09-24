93898008fd202b1916b95b35dedf2f7907bb88fb
Duncan Ryan <duncancryan1@gmail.com>
2020-09-24 14:45:46 +0100

    deck populates

--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -19,4 +19,10 @@ public class DealerTest {
     public void dealerHasDeck(){
         assertEquals(deck, dealer.getDeck());
     }
+
+    @Test
+    public void dealerDeckIsPopulated(){
+        int deckNum = dealer.getDeck().getNumberOfCards();
+        assertEquals(52, deckNum);
+    }
 }
