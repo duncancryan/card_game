f0385146d1a9f6b1242ee25f870ecc4cec7f2dcb
Duncan Ryan <duncancryan1@gmail.com>
2020-09-28 02:56:23 +0100

    halfway through game function

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
 
     private Dealer dealer;
@@ -36,15 +38,40 @@ public class Game {
         }
     }
 //
-//    public String playGame() {
+    public String playGame() {
 //        start game
-//        this.dealer.startGame(this.player);
+        this.dealer.startGame(this.player);
 //        get values of both player and dealer and output to console, check for bust, if either bust then other wins, if either blackjack then they win, if both blackjack then push/tie
+        if (player.checkBlackJack() && dealer.checkBlackJack()){
+            System.out.println("Push. No Winner.");
+        }
+        else if (player.checkBlackJack()){
+            System.out.println("BlackJack! Player Wins!");
+        }
+        else if (player.checkBust()){
+            System.out.println("Bust! Dealer Wins!");
+        }
+        else if (dealer.checkBlackJack()){
+            System.out.println("BlackJack! Dealer Wins");
+        }
+        else if (dealer.checkBust()){
+            System.out.println("Bust! Player Wins");
+        }
+        else{
+            System.out.println("Player hand: " + player.getHandValue());
+            System.out.println("Dealer hand: " + dealer.getHandValue());
+            Scanner playerChoice = new Scanner(System.in);
+            System.out.println("Stick or Twist?");
+            String choice = playerChoice.nextLine();
+            if ()
 
+        }
 //        if none of the above ask user if they want to stick or twist, if twist then deal them another card, check for bust or blackjack, and twist dealer if they are below 16
 
 //        check dealer for bust or blackjack if they hit, repeat until player sticks, blackjacks or busts
 
-//    }
+//        compare final hands if neither has bust or blackjacked and return winner
+
+    }
 
 }
